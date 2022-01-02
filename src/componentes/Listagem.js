e23ac841c183f1389bf10966a7615ca87dbabfbc
SamuPereira <samu.pereira17@hotmail.com>
2022-01-02 17:17:04 -0300

    versao inicial da listagem de tarefas

--- src/componentes/Listagem.js
+++ src/componentes/Listagem.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ListaVazia from '../assets/icones/lista-vazia.svg'
+import {Item} from './Item';
 
-export const Listagem = () =>{
-    return(
-        <div className='container-listagem'>
-            <img src={ListaVazia} alt="Nenhuma atividade encontrada"/>
-            <p>Você ainda não possui tarefas cadastradas!</p>
+export const Listagem = props => {
+
+    const { tarefas } = props;
+
+    return (
+        //Adiciona a classe container e verifica se existem tarefas, caso não exista adiciona classe vazia também para estilização no CSS e alinhamento de itens
+        <div className={"container-listagem" + (tarefas && tarefas.length > 0 ? "" : "vazia")}>
+
+            {tarefas && tarefas.length > 0 ?
+
+                tarefas?.map(item => <Item tarefa={item} key={item.id}/>)
+                :
+                <>
+                    <img src={ListaVazia} alt="Nenhuma atividade encontrada" />
+                    <p>Você ainda não possui tarefas cadastradas!</p>
+                </>
+            }
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Header } from '../componentes/Header';
 import { Filtros } from '../componentes/Filtros';
 import { Listagem } from '../componentes/Listagem';
 import { Footer } from '../componentes/Footer';
 
 export const Home = props => {
+
+    //Simulação de tarefas recebidas da API, sendo passadas via Props para o componente lista de tarefas
+    const [tarefas, setTarefas] = useState([
+        {
+            id: '123fafa',
+            nome: 'Tarefa Mock 1',
+            dataPrevisaoConclusao: '2021-07-03',
+            dataConclusao: null
+        },
+
+        {
+            id: 'samnu456',
+            nome: 'Tarefa Mock 2',
+            dataPrevisaoConclusao: '2021-07-06',
+            dataConclusao: '2021-06-30'
+        }
+    ]);
 
     const sair = () => {
         //Ao clicar em sair, limpa os dados de token do navegador para voltar a página de login
@@ -19,7 +36,7 @@ export const Home = props => {
         <>
             <Header sair={sair}/>
             <Filtros />    
-            <Listagem />
+            <Listagem tarefas={tarefas}/>
             <Footer />
         </>
     );
